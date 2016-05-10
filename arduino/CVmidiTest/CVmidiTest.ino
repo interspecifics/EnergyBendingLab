16755b460c84cfd7052f2e04fc8c9257a5e998ed
Microhom <microhom@gmail.com>
2016-05-09 21:35:45 -0500

    código con pitchbend

---
+++ arduino/CVmidiTest/CVmidiTest.ino
@@ -0,0 +1,59 @@
+#include <MIDI.h>
+int CV1 = A0;
+int CV2 = A1;
+int CV3 = A2;
+
+int led = 8;
+int led1 = 10;
+int led2 = 11;
+
+
+const int channel = 1;
+int CV1val;
+int CV2val;
+int CV3val;
+
+int sensorValue1 = 0;
+int sensorValue2 = 0;
+int sensorValue3 = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  MIDI.begin();
+  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+
+
+}
+
+void loop() {
+
+  //CONTROLAR LOS LEDS CON CADA UNO DE LOS INPUTS PARA VISUALIZAR LA SENAL
+ digitalWrite(led, HIGH);
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+  int note;
+
+  CV1val = map(CV1, 0, 1023, 22, 97);
+  CV2val = map(CV2, 0, 1023, 22, 97);
+  CV3val = map(CV3, 0, 1023, 4192, 15383);
+
+
+  // put your main code here, to run repeatedly:
+  sensorValue1 = analogRead(CV1);
+  sensorValue2 = analogRead(CV2);
+  CV1val = note;
+  //enviar la nota que se genero con la velocida de CV2
+  MIDI.sendNoteOn(sensorValue1 / 30, sensorValue2 / 127, channel);
+
+ 
+  //const int value = inPitchValue * MIDI_PITCHBEND_MAX * Settings::Toto;
+  MIDI.sendPitchBend(CV3val, channel);
+  MIDI.send(AfterTouchPoly, sensorValue1 / 30, CV2val, channel);
+  delay(100);
+  MIDI.sendNoteOff(sensorValue1 / 30, sensorValue2 / 127, channel);
+
+}
